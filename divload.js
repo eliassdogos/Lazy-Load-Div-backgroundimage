ebd232feb17a58e951f2419e9ec4d729b9f50ab8
elias <elsdogos@gmail.com>
2018-04-29 11:50:52 +0300

    Fix Variable in Js

--- divload.js
+++ divload.js
@@ -1,14 +1,14 @@
-            window.addEventListener('load', function(){
+   window.addEventListener('load', function(){
                
-                var alldivimages= document.getElementsByClassName("lazybg");
-                        for (var i=0; i<allproimages.length; i++) {
-                            //console.log(allproimages[i]);
-                            if (alldivimages[i].getAttribute('data-pic')) {
+               var alldivimages= document.getElementsByClassName("lazybg");
+                       for (var i=0; i<alldivimages.length; i++) {
+                           //console.log(allproimages[i]);
+                           if (alldivimages[i].getAttribute('data-pic')) {
 
-                                    var tt=alldivimages[i].getAttribute('data-pic');
-                                    allproimages[i].setAttribute('style',tt);
-                                    //console.log(tt);
+                                   var tt=alldivimages[i].getAttribute('data-pic');
+                                   alldivimages[i].setAttribute('style',tt);
+                                   //console.log(tt);
 
-                                    }
-                        }
-            }, false)
+                                   }
+                       }
+           }, false)
